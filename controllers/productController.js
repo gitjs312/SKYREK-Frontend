51fe6e1c28ad5ec806db97b004ebc649c8d71f63
gitjs321 <githubpractice181@gmail.com>
2026-02-21 19:30:38 +0530

    day-08-completed

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,9 @@
+
+
 export function createProduct(req, res) {
+
+    console.user(req.user)
+
     res.json({
         message: "product created successfully"
     });
--- index.js
+++ index.js
@@ -2,28 +2,28 @@ import express from 'express';
 import mongoose from "mongoose";
 import userRouter from './router/userRouter.js';
 import productRouter from './router/productRouter.js';
+import jwt from "jsonwebtoken";
 
-const mongoURI = "mongodb+srv://admin:1234@cluster0.vt2ztsl.mongodb.net/?appName=Cluster0"
 
-mongoose.connect(mongoURI).then(
-    ()=>{
-        console.log("connected to mongo db...")
-    }
-).catch(
-    (error)=>{
+const mongoURI = "mongodb+srv://admin:1234@cluster0.vt2ztsl.mongodb.net/?appName=Cluster0";
+
+mongoose.connect(mongoURI)
+    .then(() => {
+        console.log("connected to mongo db...");
+    })
+    .catch((error) => {
         console.log("Error connecting mongo DB", error);
-    }
-)
+    });
 
-const app=express();
+const app = express();
 
 app.use(express.json());
 
-app.use("/users", userRouter)
+app.use();
 
-app.use("/products", productRouter)
+app.use("/users", userRouter);
+app.use("/products", productRouter);
 
-
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('Server is running on port 3000.....');
 });
